49071c205d35aecb037c0ca04bdcf6be0c9b8c3d
Laura Flaquer <laura.flaquer@outlook.fr>
2019-11-18 00:57:26 +0100

    Final version lab1

--- handles.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    serverHandle: function (req, res) {...} 
-  }
--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
+// Import a module
+const http = require('http')
 const url = require('url')
 const qs = require('querystring')
-const http = require('http')
 
 const serverHandle = function (req, res) {
   const route = url.parse(req.url)
@@ -9,14 +10,23 @@ const serverHandle = function (req, res) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello Mr' + params['name'])
-  } else {
-    res.write('Hello anonymous')
+  if(path==='/'){
+    res.write('Explanation of how /hello works: \n- If you pass the name Charlene in parameter: it will display a short presentation of this person\n- Any other name will display: "Hello name"\n- Anything else will display: error 404 \n' )
+  }
+  else if (path === '/hello' && 'name' in params ) {
+    if(params['name']==='Charlene'){
+      res.write('Charlene is a 21 year old french student.')
+    }else{
+      res.write('Hello ' + params['name'])
+    }
+  } 
+  else{
+    res.write('error 404')
   }
 
   res.end();
 }
+// Declare an http server
+http.createServer(serverHandle).listen(8080)
 
-const server = http.createServer(serverHandle);
-server.listen(8080)
+// curl localhost:8080 or go to http://localhost:8080
